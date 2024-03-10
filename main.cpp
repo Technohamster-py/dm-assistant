70bf6e2516427a980ae68e555db4d149b20c75e8
Technohamster <arseniy.khomchenko@gmail.com>
2024-03-10 21:33:05 +0300

    Иконки плеера теперь загружаются через файл ресурсов

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <QApplication>
 #include <QPushButton>
 
+#include "qplayer.h"
+
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
-    button.show();
+    QPlayer player;
+    player.show();
     return QApplication::exec();
 }
