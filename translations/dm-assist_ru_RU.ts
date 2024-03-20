0eaddcd62d17330227f7062f1105ff84fa74453c
Technohamster <arseniy.khomchenko@gmail.com>
2024-03-20 12:07:37 +0300

    Созданы файлы перевода

---
+++ translations/dm-assist_ru_RU.ts
@@ -0,0 +1,90 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>QPlayer</name>
+    <message>
+        <location filename="../qplayer.ui" line="20"/>
+        <source>QPlayer</source>
+        <translation>QPlayer</translation>
+    </message>
+    <message>
+        <location filename="../qplayer.ui" line="57"/>
+        <source>num</source>
+        <translation>num</translation>
+    </message>
+    <message>
+        <location filename="../qplayer.ui" line="69"/>
+        <source>Title</source>
+        <translation>Название</translation>
+    </message>
+    <message>
+        <location filename="../qplayer.ui" line="95"/>
+        <source>previous track</source>
+        <translation>предыдущий трек</translation>
+    </message>
+    <message>
+        <location filename="../qplayer.ui" line="109"/>
+        <source>stop</source>
+        <translation>Стоп</translation>
+    </message>
+    <message>
+        <location filename="../qplayer.ui" line="123"/>
+        <source>play</source>
+        <translation>Пуск</translation>
+    </message>
+    <message>
+        <location filename="../qplayer.ui" line="137"/>
+        <source>pause</source>
+        <translation>Пауза</translation>
+    </message>
+    <message>
+        <location filename="../qplayer.ui" line="151"/>
+        <source>next track</source>
+        <translation>Следующий трек</translation>
+    </message>
+    <message>
+        <location filename="../qplayer.ui" line="165"/>
+        <source>Edit playlist</source>
+        <translation>Редактировать плейлист</translation>
+    </message>
+</context>
+<context>
+    <name>QPlaylistEdit</name>
+    <message>
+        <location filename="../qplaylistedit.ui" line="14"/>
+        <source>QPlaylistEdit</source>
+        <translation>QPlaylistEdit</translation>
+    </message>
+    <message>
+        <location filename="../qplaylistedit.ui" line="57"/>
+        <source>Add audio file</source>
+        <translation>Добавить аудиофайл</translation>
+    </message>
+    <message>
+        <location filename="../qplaylistedit.ui" line="71"/>
+        <source>Remove file from playlist</source>
+        <translation>Удалить файл из плейлиста</translation>
+    </message>
+    <message>
+        <location filename="../qplaylistedit.cpp" line="23"/>
+        <source>Track title</source>
+        <translation>Название трека</translation>
+    </message>
+    <message>
+        <location filename="../qplaylistedit.cpp" line="23"/>
+        <source>File path</source>
+        <translation>Путь к файлу</translation>
+    </message>
+    <message>
+        <location filename="../qplaylistedit.cpp" line="41"/>
+        <source>Add tracks</source>
+        <translation>Дбавиьт треки</translation>
+    </message>
+    <message>
+        <location filename="../qplaylistedit.cpp" line="43"/>
+        <source>Audio files (*.mp3)</source>
+        <translation>аудиофайлы (*mp3)</translation>
+    </message>
+</context>
+</TS>
