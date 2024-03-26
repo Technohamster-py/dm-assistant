a2a76900838cb52878d382ba47c24e72326bf8ef
Technohamster <arseniy.khomchenko@gmail.com>
2024-03-26 09:48:41 +0300

    Ресурсы плеера перемещены в отдельную ветку в файле ресурсов

--- translations/dm-assist_en_US.ts
+++ translations/dm-assist_en_US.ts
@@ -5,46 +5,64 @@
     <name>QPlayer</name>
     <message>
         <location filename="../qplayer.ui" line="20"/>
+        <location filename="../cmake-build-debug/DM-assist_autogen/include/ui_QPlayer.h" line="139"/>
+        <location filename="../cmake-build-debug/DM_assist_autogen/include/ui_QPlayer.h" line="139"/>
         <source>QPlayer</source>
         <translation>QPlayer</translation>
     </message>
     <message>
         <location filename="../qplayer.ui" line="57"/>
+        <location filename="../cmake-build-debug/DM-assist_autogen/include/ui_QPlayer.h" line="140"/>
+        <location filename="../cmake-build-debug/DM_assist_autogen/include/ui_QPlayer.h" line="140"/>
         <source>num</source>
         <translation>num</translation>
     </message>
     <message>
         <location filename="../qplayer.ui" line="69"/>
+        <location filename="../cmake-build-debug/DM-assist_autogen/include/ui_QPlayer.h" line="141"/>
+        <location filename="../cmake-build-debug/DM_assist_autogen/include/ui_QPlayer.h" line="141"/>
         <source>Title</source>
         <translation>Title</translation>
     </message>
     <message>
         <location filename="../qplayer.ui" line="95"/>
+        <location filename="../cmake-build-debug/DM-assist_autogen/include/ui_QPlayer.h" line="143"/>
+        <location filename="../cmake-build-debug/DM_assist_autogen/include/ui_QPlayer.h" line="143"/>
         <source>previous track</source>
         <translation>previous track</translation>
     </message>
     <message>
         <location filename="../qplayer.ui" line="109"/>
+        <location filename="../cmake-build-debug/DM-assist_autogen/include/ui_QPlayer.h" line="147"/>
+        <location filename="../cmake-build-debug/DM_assist_autogen/include/ui_QPlayer.h" line="147"/>
         <source>stop</source>
         <translation>Stop</translation>
     </message>
     <message>
         <location filename="../qplayer.ui" line="123"/>
+        <location filename="../cmake-build-debug/DM-assist_autogen/include/ui_QPlayer.h" line="151"/>
+        <location filename="../cmake-build-debug/DM_assist_autogen/include/ui_QPlayer.h" line="151"/>
         <source>play</source>
         <translation>Play</translation>
     </message>
     <message>
         <location filename="../qplayer.ui" line="137"/>
+        <location filename="../cmake-build-debug/DM-assist_autogen/include/ui_QPlayer.h" line="155"/>
+        <location filename="../cmake-build-debug/DM_assist_autogen/include/ui_QPlayer.h" line="155"/>
         <source>pause</source>
         <translation>Pause</translation>
     </message>
     <message>
         <location filename="../qplayer.ui" line="151"/>
+        <location filename="../cmake-build-debug/DM-assist_autogen/include/ui_QPlayer.h" line="159"/>
+        <location filename="../cmake-build-debug/DM_assist_autogen/include/ui_QPlayer.h" line="159"/>
         <source>next track</source>
         <translation>next track</translation>
     </message>
     <message>
         <location filename="../qplayer.ui" line="165"/>
+        <location filename="../cmake-build-debug/DM-assist_autogen/include/ui_QPlayer.h" line="163"/>
+        <location filename="../cmake-build-debug/DM_assist_autogen/include/ui_QPlayer.h" line="163"/>
         <source>Edit playlist</source>
         <translation>Edit playlist</translation>
     </message>
@@ -53,16 +71,22 @@
     <name>QPlaylistEdit</name>
     <message>
         <location filename="../qplaylistedit.ui" line="14"/>
+        <location filename="../cmake-build-debug/DM-assist_autogen/include/ui_QPlaylistEdit.h" line="105"/>
+        <location filename="../cmake-build-debug/DM_assist_autogen/include/ui_QPlaylistEdit.h" line="105"/>
         <source>QPlaylistEdit</source>
         <translation>QPlaylistEdit</translation>
     </message>
     <message>
         <location filename="../qplaylistedit.ui" line="57"/>
+        <location filename="../cmake-build-debug/DM-assist_autogen/include/ui_QPlaylistEdit.h" line="107"/>
+        <location filename="../cmake-build-debug/DM_assist_autogen/include/ui_QPlaylistEdit.h" line="107"/>
         <source>Add audio file</source>
         <translation>Add audio file</translation>
     </message>
     <message>
         <location filename="../qplaylistedit.ui" line="71"/>
+        <location filename="../cmake-build-debug/DM-assist_autogen/include/ui_QPlaylistEdit.h" line="111"/>
+        <location filename="../cmake-build-debug/DM_assist_autogen/include/ui_QPlaylistEdit.h" line="111"/>
         <source>Remove file from playlist</source>
         <translation>Remove file from playlist</translation>
     </message>
