81b73867758feda1dddc07c9aae2ce6bbc2d6b96
aakhomchenko <homchenko_aa@nrcki.ru>
2024-04-04 13:23:47 +0300

    Описан класс EncounterEntity и Encounter

--- encounter.cpp
+++ encounter.cpp
@@ -5,6 +5,8 @@
 #include "encounter.h"
 
 Encounter::Encounter() {
+    m_encounterModel = new QStandardItemModel();
+    m_encounterModel->setHorizontalHeaderLabels(QStringList() << "" << "" << "AC" << "HP");
 
 }
 
@@ -13,9 +15,84 @@ Encounter::~Encounter() {
 }
 
 void Encounter::addCharacter(Character character) {
+    EncounterEntity characterEntity(&character, false);
 
+    QList<QStandardItem *> items;
+    items.append(new QStandardItem(characterEntity.getInitiativeValue()));
+    items.append(new QStandardItem(characterEntity.getTitle()));
+    items.append(new QStandardItem(characterEntity.getAC()));
+
+    QString hpToMaxHp = QString::number(characterEntity.getHP()) + "/" + QString::number(characterEntity.getMaxHp());
+
+    items.append(new QStandardItem(hpToMaxHp));
+    m_encounterModel->appendRow(items);
+
+    m_entities.push_back(characterEntity);
 }
 
 void Encounter::addMonster(Monster monster) {
+    EncounterEntity monsterEntity(&monster, false);
+
+    QList<QStandardItem *> items;
+    items.append(new QStandardItem(monsterEntity.getInitiativeValue()));
+    items.append(new QStandardItem(monsterEntity.getTitle()));
+    items.append(new QStandardItem(monsterEntity.getAC()));
+
+    QString hpToMaxHp = QString::number(monsterEntity.getHP()) + "/" + QString::number(monsterEntity.getMaxHp());
+
+    items.append(new QStandardItem(hpToMaxHp));
+    m_encounterModel->appendRow(items);
+
+    m_entities.push_back(monsterEntity);
+}
+
+
+EncounterEntity::EncounterEntity(Character *entity, int initiativeRoll, bool autoAddBonus) {
+    m_type = character;
+    m_title = entity->getTitle();
+    m_character = entity;
+    m_characterSheetId = entity->getCharacterSheetId();
+    m_ac = entity->getAc();
+    m_hp = entity->getHp();
+    m_maxHp = entity->getMaxHp();
+    m_initiativeBonus = entity->getInitiativeBonus();
+
+    setInitiativeValue(initiativeRoll, autoAddBonus);
+}
+
+EncounterEntity::EncounterEntity(Monster *entity, int initiativeRoll, bool autoAddBonus) {
+    m_type = monster;
+    m_characterSheetId = entity->getCharacterSheetId();
+    m_ac = entity->getAc();
+    m_hp = entity->getHp();
+    m_maxHp = entity->getMaxHp();
+    m_initiativeBonus = entity->getInitiativeBonus();
+
+    setInitiativeValue(initiativeRoll, autoAddBonus);
+}
+
+EncounterEntity::~EncounterEntity() {
 
 }
+
+void EncounterEntity::setInitiativeValue(int initiativeRoll, bool autoAddBonus) {
+    if(autoAddBonus)
+        m_initiativeValue = initiativeRoll + m_initiativeBonus;
+    else
+        m_initiativeValue = initiativeRoll;
+}
+
+void EncounterEntity::setHp(int value) {
+    if(value <= m_maxHp){
+        if(m_type == character)
+            m_character->setHp(value);
+        m_hp = value;
+    }
+}
+
+void EncounterEntity::setMaxHp(int value) {
+    if(m_type == character)
+        m_character->setMaxHp(value);
+    m_maxHp = value;
+}
+
--- encounter.h
+++ encounter.h
@@ -7,6 +7,43 @@
 
 #include "character.h"
 #include "monster.h"
+#include <QStandardItemModel>
+
+class EncounterEntity{
+public:
+    explicit EncounterEntity(Character *entity, int initiativeRoll = 0, bool autoAddBonus = false);
+    explicit EncounterEntity(Monster *entity, int initiativeRoll = 0, bool autoAddBonus = false);
+
+    ~EncounterEntity();
+
+    enum Type{
+        monster = 0,
+        character = 1
+    };
+
+    [[nodiscard]] int getInitiativeValue() const {return m_initiativeValue;};
+    [[nodiscard]] int getAC() const {return m_ac;};
+    [[nodiscard]] int getHP() const {return m_hp;};
+    [[nodiscard]] int getMaxHp() const {return m_maxHp;};
+    QString getTitle() const {}
+
+    void setHp(int value);
+    void setMaxHp(int value);
+    void setInitiativeValue(int initiativeRoll, bool autoAddBonus);
+
+
+private:
+    int m_type;
+    Character *m_character;
+
+    QString m_title = "";
+    int m_characterSheetId = 0;
+    int m_initiativeBonus = 0;
+    int m_initiativeValue = 0;
+    int m_ac = 10;
+    int m_hp = 0;
+    int m_maxHp = 0;
+};
 
 class Encounter {
 public:
@@ -17,8 +54,8 @@ public:
     void addMonster(Monster monster);
 
 private:
-    QVector<Character> m_characters;
-    QVector<Monster> m_monsters;
+    QStandardItemModel *m_encounterModel;
+    QVector<EncounterEntity> m_entities;
 };
 
 
