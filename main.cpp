886970d0082d7625a36c064dd5fa88ed4b7a97c8
Technohamster <arseniy.khomchenko@gmail.com>
2024-04-10 20:57:22 +0300

    Иызменены типы виджетов с qplayer на qwidget

--- main.cpp
+++ main.cpp
@@ -1,10 +1,55 @@
 #include <QApplication>
-#include <QPushButton>
 
+#include "random"
+#include "qinitiativetrackerwidget.h"
 #include "encounter.h"
 
+#include <QDebug>
+
+
 int main(int argc, char *argv[]) {
+    std::random_device rd;
+    std::mt19937 d20(rd());
+    std::uniform_int_distribution <> dis(1, 20);
+
     QApplication a(argc, argv);
+    Encounter encounter("Боссфайт");
+
+    Monster monster1("Werwolf");
+    monster1.setAc(15);
+    monster1.setMaxHp(48);
+    monster1.setFullHp();
+    monster1.setInitiativeBonus(-2);
+
+    Monster monster2("Dolboeb");
+    monster2.setAc(13);
+    monster2.setMaxHp(100);
+    monster2.setFullHp();
+    monster2.setInitiativeBonus(3);
+
+
+    Character char1("Gandalf");
+    char1.setAc(18);
+    char1.setMaxHp(78);
+    char1.setFullHp();
+    char1.setInitiativeBonus(2);
+
+
+    Character char2("Shrek");
+    char2.setAc(16);
+    char2.setMaxHp(53);
+    char2.setFullHp();
+    char2.setInitiativeBonus(5);
+
+
+    encounter.addCharacter(&char1, dis(d20), true);
+    encounter.addCharacter(&char2, dis(d20), true);
+    encounter.addMonster(&monster1, dis(d20), true);
+    encounter.addMonster(&monster2, dis(d20), true);
+
+    QInitiativeTrackerWidget initiativeTrackerWidget;
+    initiativeTrackerWidget.loadEncounter(&encounter);
+    initiativeTrackerWidget.show();
 
     return QApplication::exec();
 }
